ae6d7a6cc65436655f9dbfebe8f6741fbcea5886
Ramses Miramontes Meza <ramses7083@MacBook-Pro-de-Ramses.local>
2015-05-05 17:08:31 -0500

    Initial Commit

---
+++ Modi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Modi
+//
+//  Created by Ramses Miramontes Meza on 05/05/15.
+//  Copyright (c) 2015 RASOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
